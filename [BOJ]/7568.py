40a9a7e9511bb64cb3cb36b94a3c1356ec26d8d7
origin <judah235@naver.com>
2022-06-04 02:34:55 +0900

    [BOJ]/덩치/실버5
    https://www.acmicpc.net/problem/7568

---
+++ [BOJ]/7568.py
@@ -0,0 +1,16 @@
+n=int(input())
+
+l=[]
+ans=[]
+for _ in range(n):
+  l.append(list(map(int,input().split())))
+
+for i in range(n):
+  count=0
+  for j in range(n):
+    if l[i][0]<l[j][0] and l[i][1]<l[j][1]:
+      count+=1
+  ans.append(count+1)
+
+for i in ans:
+  print(i,end=' ')
